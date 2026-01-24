0417e45464b7912cd5e1a50a934a8f37e73d0655
MarcusKymmell <marcuskymmell@gmail.com>
2026-01-24 21:24:13 +0100

    fileio

---
+++ Fileio/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Fileio;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+namespace _1_FileIo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run();
+    }
+
+    void Run()
+    {
+        Console.WriteLine(Environment.CurrentDirectory);
+
+    string[] lines = System.IO.File.ReadAllLines("quiz.txt");
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+
+    string[] linesNieuws = System.IO.File.ReadAllLines("nieuws.txt");
+
+        foreach (string line in linesNieuws)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
